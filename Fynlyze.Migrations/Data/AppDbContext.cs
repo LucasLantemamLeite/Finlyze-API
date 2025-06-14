74b5c865a9799ba3f093c86dd9f05ca4a4381e64
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-06-14 15:23:36 -0300

    chore: movido a connection string para o appsettings da API e removido comentário do construtor

--- Fynlyze.Migrations/Data/AppDbContext.cs
+++ Fynlyze.Migrations/Data/AppDbContext.cs
@@ -4,9 +4,7 @@ namespace Finlyze.Migrations.Data.Context;
 
 public class AppDbContext : DbContext
 {
-    // public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=FinlyzeDb;User Id=sa;Password=Lucas1971!;TrustServerCertificate=true;");
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
 
